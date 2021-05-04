a7232e1de17d2e7c882f0a2eca96739eae0a3431
Tiandians <zhubaolin228@gmail.com>
2021-05-04 10:07:15 +0800

    5.4

---
+++ OpenGLProject/Collision_event.h
@@ -0,0 +1,54 @@
+﻿#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "Object.h"
+#include <vector>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <GLFW\glfw3.h>
+constexpr auto DELTATIME = 0.1f;
+extern int sumbounce;
+extern int sumexam;
+
+
+class Object; 
+class MovableObject;
+class FixedBall; 
+class Wall;
+class Ball;
+
+class CollisionSystem;
+
+
+class CollisionSystem
+{
+    friend void display(GLFWwindow *window, double currentTime, CollisionSystem &system);
+    friend std::istream &operator>>(std::istream &, CollisionSystem &);
+    friend std::ostream &operator<<(std::ostream &, CollisionSystem &);
+
+public:
+    //construct and destruct
+    CollisionSystem() = default;
+    ~CollisionSystem() = default;
+    CollisionSystem(std::istream &is) : CollisionSystem() { is >> *this; }
+
+    //action
+    void run(float);
+    void reverse();
+    std::vector<std::shared_ptr<Ball>>& b() { return balls; }
+    std::vector<std::shared_ptr<FixedBall>>& fb() { return fixedBalls; }
+    std::vector<std::shared_ptr<Wall>>& w() { return walls; }
+
+private:
+    void init();
+    void move(float);
+    std::vector<std::shared_ptr<Ball>> balls;
+    std::vector<std::shared_ptr<FixedBall>> fixedBalls;
+    std::vector<std::shared_ptr<Wall>> walls;
+    float currentTime = 0;
+};
+std::istream &operator>>(std::istream &, CollisionSystem &);
+std::ostream &operator<<(std::ostream &, CollisionSystem &);
+
+#endif
---
+++ OpenGLProject/vs_plane.glsl
@@ -0,0 +1,12 @@
+#version 430
+
+layout (location = 0) in vec3 position;
+
+uniform mat4 v_matrix;
+uniform mat4 p_matrix;
+uniform mat4 r_matrix;
+
+void main(void)
+{
+    gl_Position = p_matrix*v_matrix*vec4(position,1.0);
+}
