9cb1923f29c292f474ab89be2d4f1aed3af3cc89
朱宝林 <qq1614510917@163.com>
2021-05-26 21:12:54 +0800

    5.26

--- OpenGLProject/gen_gas.cpp
+++ OpenGLProject/gen_gas.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
@@ -7,24 +7,31 @@
 #include <cmath>
 
 using namespace std;
+#define LINE 16//边排列个数
+#define NUM 1024//小球个数
+#define DISTANCE 1.549733e-09//小球间距
+#define MASS 1.0//小球质量
+#define RADIUS 0.5//小球半径
+#define MEAN 0//均值
+#define DELTA 2.664195e+02//方差
 
 int main()
 {
     ofstream ofstrm("testdata.txt");
     std::default_random_engine e;                        //引擎
-    std::normal_distribution<double> n(0, 2.664195e+02); //均值, 方差
-    for (int i = 0; i != 16; i++)
-        for (int j = 0; j != 16; j++)
-            for (int k = 0; k != 16; k++)
+    std::normal_distribution<double> n(MEAN,DELTA); 
+    for (int i = 0; i != LINE; i++)
+        for (int j = 0; j != LINE; j++)
+            for (int k = 0; k != LINE; k++)
             {
-                ofstrm << i * 1.549733e-09 << ' '
-                     << j * 1.549733e-09 << ' '
-                     << k * 1.549733e-09 << ' '
+                ofstrm << i * DISTANCE << ' '
+                     << j * DISTANCE << ' '
+                     << k * DISTANCE << ' '
                      << n(e) << ' '
                      << n(e) << ' '
                      << n(e) << ' '
-                     << 5.313393e-26 << ' '
-                     << 0.346e-9
+                     << MASS << ' '
+                     << RADIUS
                      << endl;
             }
     return 0;
--- README.md
+++ README.md
@@ -10,23 +10,6 @@
 
 ~~将`OpenGLtemplate`文件夹复制到`C:\`~~
 
-## Todo List
-
-### 当前开发进度
-
-已经完成球与球之间的碰撞
-
-等待做的：
-
-* 循环全部使用迭代器
-* 全部改成智能指针
-
-遗留问题：
-
-* 返回根的问题（通过反向运行判断，但未检验实用性
-* 步长的问题，留到事件实现中讨论
-* 测试小球和墙的碰撞（如果不行还要修复）
-
 ## 依赖项
 
 * [GLFW:Graphics Library Framework](https://github.com/glfw/glfw)
@@ -34,7 +17,7 @@
 * [GLM:OpenGL Mathematics](https://github.com/g-truc/glm)
 * [SOIL2:Simple OpenGL Image Library](https://github.com/SpartanJ/soil2)
 
-为缩减项目体积，已经删去Optional部分的库，仅保留必要设施
+为缩减项目体积，已经删去Optional部分的库
 
 Optional:
 
@@ -42,7 +25,5 @@ Optional:
 * [GLAD help](https://blog.csdn.net/sigmarising/article/details/80470054)
 * [freeglut](https://www.transmissionzero.co.uk/software/freeglut-devel/)
 * [freeglut help](https://www.2bboy.com/archives/181.html)
-
-~~*GLFW GLEW GLM SOIL2 GLAD freeglut全部都已经配置好*放在OpenGLtemplate中，~~
 
 默认配置为x64，使用OpenGL4.3(GLAD选项).使用GLAD时，请在项目源文件中包含`OpenGLtemplate\glad_src\glad.c`
