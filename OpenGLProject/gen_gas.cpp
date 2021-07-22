0cc2c75f489a0f593a550da06ec5f645bd72a5d0
Tiandians <zhubaolin228@gmail.com>
2021-07-22 16:08:30 +0800

    data 6

--- OpenGLProject/gen_gas.cpp
+++ OpenGLProject/gen_gas.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-#define NUM 16//边个数
-#define DISTANCE 0.5//间距
-#define MASS 1//质量
-#define RADIUS 0.5//半径
-#define MEAN 0//均值
-#define DELTA 1//方差
+#define NUM 89//边个数
+#define DISTANCE 3.353473e-9//间距
+#define MASS 5.3e-26//质量
+#define RADIUS 0.173e-9//半径
+#define MEAN 0.0//均值
+#define DELTA 2.667499e2//方差
 
 int main()
 {
@@ -17,11 +17,11 @@ int main()
     std::default_random_engine e;                        //引擎
     std::normal_distribution<double> n(MEAN,DELTA);
     e.seed(time(0));
-    double origin = -(RADIUS + DISTANCE / 2.0) * (NUM - 1);
-    double distance = 2 * RADIUS + DISTANCE;
+    double origin = -(DISTANCE* (NUM - 1))/2.0;
+    double distance = DISTANCE;
     ofstrm << "C 1" << endl
            << 0 << '\t' << 0 << '\t' << 0 << endl
-           << 1.5 * (NUM + 1) << '\t' << 1.5 * (NUM + 1) << '\t' << 1.5 * (NUM + 1) << endl;
+           << 0.3e-6 << '\t' << 0.3e-6 << '\t' << 0.3e-6 << endl;
     ofstrm << "B " << NUM * NUM * NUM << endl;
     for (int i = 0; i != NUM; i++)
         for (int j = 0; j != NUM; j++)
